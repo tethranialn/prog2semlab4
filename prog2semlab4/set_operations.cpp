ea3f00574cfe6fd88b9863a437b998ac7e765e66
tethranialn <159460032+tethranialn@users.noreply.github.com>
2025-05-18 20:18:56 +0300

    done?

--- prog2semlab4/set_operations.cpp
+++ prog2semlab4/set_operations.cpp
@@ -2,80 +2,90 @@
 #include "list.h"
 #include "copyS.h"
 
-// Вспомогательная функция для получения следующего элемента из списка
-static const char* GetNextElement(EL_V*& currentLevel, EL_Stroka*& currentElem) {
-    while (currentLevel) {
-        if (currentElem) {
-            const char* value = currentElem->A;
-            currentElem = currentElem->next;
-            return value;
-        }
-        currentLevel = currentLevel->next;
-        if (currentLevel) currentElem = currentLevel->F.h;
+#define MAX_FULL_STR 1024
+
+static void GetFullString(const EL_V* level, char* buffer, unsigned max) {
+    if (!level || max == 0) {
+        buffer[0] = '\0';
+        return;
     }
-    return nullptr;
+    unsigned pos = 0;
+    EL_Stroka* current = level->F.h;
+    while (current && pos < max - 1) {
+        const char* part = current->A;
+        unsigned i = 0;
+        while (pos < max - 1 && part[i] != '\0') {
+            buffer[pos++] = part[i++];
+        }
+        current = current->next;
+    }
+    buffer[pos] = '\0';
 }
 
 void Intersection(const Form_V& s1, const Form_V& s2, Form_V& result) {
     InitFormV(result);
 
     EL_V* level1 = s1.h;
-    EL_Stroka* elem1 = level1 ? level1->F.h : nullptr;
-    const char* current1 = GetNextElement(level1, elem1);
-
     EL_V* level2 = s2.h;
-    EL_Stroka* elem2 = level2 ? level2->F.h : nullptr;
-    const char* current2 = GetNextElement(level2, elem2);
 
-    EL_V* resLevel = nullptr;
-    while (current1 && current2) {
-        int cmp = CompareStrings(current1, current2);
+    while (level1 && level2) {
+        char str1[MAX_FULL_STR];
+        char str2[MAX_FULL_STR];
+        GetFullString(level1, str1, MAX_FULL_STR);
+        GetFullString(level2, str2, MAX_FULL_STR);
+
+        int cmp = CompareStrings(str1, str2);
 
         if (cmp == 0) {
-            if (!resLevel || resLevel->F.L >= 3) {
-                EL_V* newLevel = new EL_V;
-                InitELV(newLevel);
-                if (!result.h) result.h = result.last = newLevel;
-                else result.last->next = newLevel;
-                result.last = newLevel;
-                resLevel = newLevel;
+            EL_V* newLevel = new EL_V;
+            InitELV(newLevel);
+            EL_Stroka* current = level1->F.h;
+            while (current) {
+                InsertString(newLevel->F, current->A);
+                current = current->next;
             }
-            InsertString(resLevel->F, current1);
-
-            current1 = GetNextElement(level1, elem1);
-            current2 = GetNextElement(level2, elem2);
+            if (!result.h) {
+                result.h = result.last = newLevel;
+            }
+            else {
+                result.last->next = newLevel;
+                result.last = newLevel;
+            }
+            level1 = level1->next;
+            level2 = level2->next;
         }
         else if (cmp < 0) {
-            current1 = GetNextElement(level1, elem1);
+            level1 = level1->next;
         }
         else {
-            current2 = GetNextElement(level2, elem2);
+            level2 = level2->next;
         }
     }
 }
 
 bool IsSubset(const Form_V& subset, const Form_V& mainSet) {
     EL_V* subLevel = subset.h;
-    EL_Stroka* subElem = subLevel ? subLevel->F.h : nullptr;
-    const char* currentSub = GetNextElement(subLevel, subElem);
-
     EL_V* mainLevel = mainSet.h;
-    EL_Stroka* mainElem = mainLevel ? mainLevel->F.h : nullptr;
-    const char* currentMain = GetNextElement(mainLevel, mainElem);
 
-    while (currentSub && currentMain) {
-        int cmp = CompareStrings(currentSub, currentMain);
+    while (subLevel && mainLevel) {
+        char subStr[MAX_FULL_STR];
+        char mainStr[MAX_FULL_STR];
+        GetFullString(subLevel, subStr, MAX_FULL_STR);
+        GetFullString(mainLevel, mainStr, MAX_FULL_STR);
+
+        int cmp = CompareStrings(subStr, mainStr);
 
         if (cmp == 0) {
-            currentSub = GetNextElement(subLevel, subElem);
-            currentMain = GetNextElement(mainLevel, mainElem);
+            subLevel = subLevel->next;
+            mainLevel = mainLevel->next;
         }
         else if (cmp < 0) {
             return false;
         }
         else {
-            currentMain = GetNextElement(mainLevel, mainElem);
+            mainLevel = mainLevel->next;
         }
     }
-    return !currentSub;
+
+    return subLevel == nullptr;
 }
